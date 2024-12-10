a4cf170aedaba7235d814eaced530377ed71c751
Ryan Goh <ry4n.gch@gmail.com>
2024-12-10 14:21:39 +0800

    remove project in favour of original bitbucket repo

--- portfolio-site/src/app.js
+++
@@ -1,9 +0,0 @@
-import React, {Component} from 'react';
-import ReactDOM from 'react-dom';
-import AppRouter from './routers/AppRouter';
-import 'normalize.css/normalize.css';
-import './styles/styles.scss';
-
-
-
-ReactDOM.render(<AppRouter />, document.getElementById('app'));
