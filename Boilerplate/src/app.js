52743d90cd74f0b6dc753481eab5929bf327436b
Ryan Goh <ry4n.gch@gmail.com>
2024-12-11 21:58:27 +0800

    remove boilerplate in favour of bitbucket

--- Boilerplate/src/app.js
+++
@@ -1,6 +0,0 @@
-import React, {Component} from 'react';
-import ReactDOM from 'react-dom';
-import 'normalize.css/normalize.css';
-import './styles/styles.scss';
-
-ReactDOM.render(<p>This is my boilerplate</p>, document.getElementById('app'));
